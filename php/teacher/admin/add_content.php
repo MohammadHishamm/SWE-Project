f29428a410d620558dc6a3a3a62e19ba7bbfa617
Mark <mark2107794@miuegypt.edu.eg>
2023-11-18 17:27:29 +0200

    "try"

--- php/teacher/admin/add_content.php
+++ php/teacher/admin/add_content.php
@@ -1,7 +1,6 @@
 <?php
-
-include '../components/connect.php';
-// include "../../dbh.inc.php";
+require_once('../components/content_control.php');
+$content = new content;
 
 if (isset($_COOKIE['tutor_id'])) {
    $tutor_id = $_COOKIE['tutor_id'];
@@ -12,7 +11,7 @@ if (isset($_COOKIE['tutor_id'])) {
 
 if (isset($_POST['submit'])) {
 
-   $id = unique_id();
+   
    $status = $_POST['status'];
    $status = filter_var($status, FILTER_SANITIZE_STRING);
    $title = $_POST['title'];
@@ -23,29 +22,42 @@ if (isset($_POST['submit'])) {
    $playlist = filter_var($playlist, FILTER_SANITIZE_STRING);
 
    $thumb = $_FILES['thumb']['name'];
-   $thumb = filter_var($thumb, FILTER_SANITIZE_STRING);
-   $thumb_ext = pathinfo($thumb, PATHINFO_EXTENSION);
-   $rename_thumb = unique_id() . '.' . $thumb_ext;
-   $thumb_size = $_FILES['thumb']['size'];
-   $thumb_tmp_name = $_FILES['thumb']['tmp_name'];
-   $thumb_folder = '../uploaded_files/' . $rename_thumb;
-
    $video = $_FILES['video']['name'];
-   $video = filter_var($video, FILTER_SANITIZE_STRING);
-   $video_ext = pathinfo($video, PATHINFO_EXTENSION);
-   $rename_video = unique_id() . '.' . $video_ext;
-   $video_tmp_name = $_FILES['video']['tmp_name'];
-   $video_folder = '../uploaded_files/' . $rename_video;
+   
+   $content->setTutorId($tutor_id);
+   $content->setContentId();
+   $content->setContentStatus($status);
+   $content->setContentTitle($title);
+   $content->setContentDescription($description);
+   $content->setContentPlaylist($playlist);
+   $content->setContentThumb($thumb);
+   $content->setContentVideo($video);
 
-   if ($thumb_size > 2000000) {
-      $message[] = 'image size is too large!';
-   } else {
-      $add_playlist = $conn->prepare("INSERT INTO `content`(id, tutor_id, playlist_id, title, description, video, thumb, status) VALUES(?,?,?,?,?,?,?,?)");
-      $add_playlist->execute([$id, $tutor_id, $playlist, $title, $description, $rename_video, $rename_thumb, $status]);
-      move_uploaded_file($thumb_tmp_name, $thumb_folder);
-      move_uploaded_file($video_tmp_name, $video_folder);
+   // echo $content->getTutorId() ."<br>";
+   // echo $content->getContentId()."<br>";
+   // echo $content->getContentStatus()."<br>";
+   // echo $content->getContentTitle()."<br>";
+   // echo $content->getContentDescription()."<br>";
+   // echo $content->getContentPlaylist()."<br>";
+   // echo $content->getContentThumb()."<br>";
+   // echo $content->getContentVideo()."<br>";
+
+   if($content->saveContent()){
       $message[] = 'new course uploaded!';
+   }else{
+      $message[] = 'failed to upload course!';
    }
+
+
+   // if ($thumb_size > 2000000) {
+   //    $message[] = 'image size is too large!';
+   // } else {
+   //    $add_playlist = $conn->prepare("INSERT INTO `content`(id, tutor_id, playlist_id, title, description, video, thumb, status) VALUES(?,?,?,?,?,?,?,?)");
+   //    $add_playlist->execute([$id, $tutor_id, $playlist, $title, $description, $rename_video, $rename_thumb, $status]);
+   //    move_uploaded_file($thumb_tmp_name, $thumb_folder);
+   //    move_uploaded_file($video_tmp_name, $video_folder);
+   //    $message[] = 'new course uploaded!';
+   // }
 }
 
 ?>
@@ -90,11 +102,13 @@ if (isset($_POST['submit'])) {
          <select name="playlist" class="box" required>
             <option value="" disabled selected>--select playlist</option>
             <?php
-            $select_playlists = $conn->prepare("SELECT * FROM `playlist` WHERE tutor_id = ?");
+            $select_playlists = $content->get_connect()->prepare("SELECT * FROM `playlist` WHERE tutor_id = ?");
             $select_playlists->execute([$tutor_id]);
             if ($select_playlists->rowCount() > 0) {
                while ($fetch_playlist = $select_playlists->fetch(PDO::FETCH_ASSOC)) {
+                  
             ?>
+
                   <option value="<?= $fetch_playlist['id']; ?>"><?= $fetch_playlist['title']; ?></option>
                <?php
                }
@@ -105,6 +119,7 @@ if (isset($_POST['submit'])) {
             }
             ?>
          </select>
+         
          <p>select thumbnail <span>*</span></p>
          <input type="file" name="thumb" accept="image/*" required class="box">
          <p>select video <span>*</span></p>
