cd0aea43da20a6f2489ddf1827ff1377e26cfa87
Omar Hosny <omar2108721@miuegypt.edu.eg>
2023-11-21 21:20:30 +0200

    display courses in admin-page

--- php/admin/courses.php
+++ php/admin/courses.php
@@ -1,3 +1,36 @@
+<?php
+
+require_once('../teacher/components/playlist_control.php');
+
+$playlist = new playlist;
+
+session_start();
+foreach($_SESSION['user_data'] as $key => $value)
+{
+   $tutor_id = $value['id'];
+}
+
+
+if(isset($_POST['delete']))
+{
+   $delete_id = $_POST['playlist_id'];
+   $delete_id = filter_var($delete_id, FILTER_SANITIZE_STRING);
+
+   if(  $playlist->Delete_playlist($delete_id,$tutor_id))
+   {
+      $message[] = 'playlist deleted!';
+   }
+   else
+   {
+      $message[] = 'playlist already deleted!';
+   }
+ 
+  
+}
+
+?>
+
+
 <!DOCTYPE php>
 <html lang="en">
 
@@ -83,164 +116,50 @@
         </div>
       </div>
       <!-- End Head -->
+
+
       <h1 class="p-relative">Courses</h1>
+      
       <div class="courses-page d-grid m-20 gap-20">
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-01.jpg" alt="" />
-          <img class="instructor" src="imgs/team-01.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">Mastering Web Design</h4>
-            <p class="description c-grey mt-15 fs-14">
-              Master The Art Of Web Designing And Mocking, Prototyping And Creating Web Design Architecture
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey">
-              <i class="fa-regular fa-user"></i>
-              950
-            </span>
-            <span class="c-grey">
-              <i class="fa-solid fa-dollar-sign"></i>
-              165
-            </span>
-          </div>
+  <?php
+  $select_courses = $playlist->get_All_playlist($tutor_id);
+  if ($select_courses->rowCount() > 0) {
+    while ($fetch_course = $select_courses->fetch(PDO::FETCH_ASSOC)) {
+      $course_id = $fetch_course['id'];
+    
+      $count_videos = $playlist->get_connect()->prepare("SELECT * FROM `content` WHERE playlist_id = ?");
+      $count_videos->execute([$course_id]);
+      $total_videos = $count_videos->rowCount();
+  ?>
+      <div class="course bg-white rad-6 p-relative">
+        <img class="cover" src="../teacher/uploaded_files/<?= $fetch_course['thumb']; ?>" alt="" />
+     
+       
+        <div class="p-20">
+          <h4 class="m-0"><?= $fetch_course['title']; ?></h4>
+          <p class="description c-grey mt-15 fs-14">
+            <?= $fetch_course['description']; ?>
+          </p>
         </div>
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-02.jpg" alt="" />
-          <img class="instructor" src="imgs/team-02.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">Data Structure And Algorithms</h4>
-            <p class="description c-grey mt-15 fs-14">
-              Master The Art Of Data Strcuture And Famous Algorithms Like Sorting, Dividing And Conquering
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey"> <i class="fa-regular fa-user"></i> 1150</span>
-            <span class="c-grey"><i class="fa-solid fa-dollar-sign"></i> 210</span>
-          </div>
+        <div class="info p-15 p-relative between-flex">
+          <span  class="title bg-blue c-white btn-shape">Course Info</span>
+          <span class="c-grey">
+            <i class="fa-regular fa-user"></i>
+            <?= $total_videos; ?> 
+          </span>
+          
         </div>
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-03.jpg" alt="" />
-          <img class="instructor" src="imgs/team-01.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">Responsive Web Design</h4>
-            <p class="description c-grey mt-15 fs-14">
-              Mastering Responsive Web Design And Media Queries And Know Everything About Breakpoints
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey"> <i class="fa-regular fa-user"></i> 650</span>
-            <span class="c-grey"><i class="fa-solid fa-dollar-sign"></i> 90</span>
-          </div>
-        </div>
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-04.jpg" alt="" />
-          <img class="instructor" src="imgs/team-03.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">Mastering Python</h4>
-            <p class="description c-grey mt-15 fs-14">
-              Mastering Python To Prepare For Data Science And AI And Automating Things in Your Life
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey"> <i class="fa-regular fa-user"></i> 950</span>
-            <span class="c-grey"><i class="fa-solid fa-dollar-sign"></i> 250</span>
-          </div>
-        </div>
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-05.jpg" alt="" />
-          <img class="instructor" src="imgs/team-03.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">PHP Examples</h4>
-            <p class="description c-grey mt-15 fs-14">
-              PHP Tutorials And Examples And Practice On Web Application And Connecting With Databases
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey"> <i class="fa-regular fa-user"></i> 850</span>
-            <span class="c-grey"><i class="fa-solid fa-dollar-sign"></i> 150</span>
-          </div>
-        </div>
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-02.jpg" alt="" />
-          <img class="instructor" src="imgs/team-02.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">Data Structure And Algorithms</h4>
-            <p class="description c-grey mt-15 fs-14">
-              Master The Art Of Data Strcuture And Famous Algorithms Like Sorting, Dividing And Conquering
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey"> <i class="fa-regular fa-user"></i> 1150</span>
-            <span class="c-grey"><i class="fa-solid fa-dollar-sign"></i> 210</span>
-          </div>
-        </div>
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-03.jpg" alt="" />
-          <img class="instructor" src="imgs/team-01.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">Responsive Web Design</h4>
-            <p class="description c-grey mt-15 fs-14">
-              Mastering Responsive Web Design And Media Queries And Know Everything About Breakpoints
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey"> <i class="fa-regular fa-user"></i> 650</span>
-            <span class="c-grey"><i class="fa-solid fa-dollar-sign"></i> 90</span>
-          </div>
-        </div>
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-01.jpg" alt="" />
-          <img class="instructor" src="imgs/team-01.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">Mastering Web Design</h4>
-            <p class="description c-grey mt-15 fs-14">
-              Master The Art Of Web Designing And Mocking, Prototyping And Creating Web Design Archticture
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey"> <i class="fa-regular fa-user"></i> 850</span>
-            <span class="c-grey"><i class="fa-solid fa-dollar-sign"></i> 145</span>
-          </div>
-        </div>
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-05.jpg" alt="" />
-          <img class="instructor" src="imgs/team-03.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">PHP Examples</h4>
-            <p class="description c-grey mt-15 fs-14">
-              PHP Tutorials And Examples And Practice On Web Application And Connecting With Databases
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey"> <i class="fa-regular fa-user"></i> 850</span>
-            <span class="c-grey"><i class="fa-solid fa-dollar-sign"></i> 150</span>
-          </div>
-        </div>
-        <div class="course bg-white rad-6 p-relative">
-          <img class="cover" src="imgs/course-04.jpg" alt="" />
-          <img class="instructor" src="imgs/team-03.png" alt="" />
-          <div class="p-20">
-            <h4 class="m-0">Mastering Python</h4>
-            <p class="description c-grey mt-15 fs-14">
-              Mastering Python To Prepare For Data Science And AI And Automating Things in Your Life
-            </p>
-          </div>
-          <div class="info p-15 p-relative between-flex">
-            <span class="title bg-blue c-white btn-shape">Course Info</span>
-            <span class="c-grey"> <i class="fa-regular fa-user"></i> 950</span>
-            <span class="c-grey"><i class="fa-solid fa-dollar-sign"></i> 250</span>
-          </div>
-        </div>
+      </div>
+  <?php
+    }
+  } else {
+    echo '<p class="empty">No courses added yet!</p>';
+  }
+  ?>
+</div>
+
+         
+       
       </div>
     </div>
   </div>
