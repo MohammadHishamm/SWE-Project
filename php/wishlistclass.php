8a8ceb1f5575933888eb07b2456449027a4e9ffe
Mohammad Hisham <mohammad2109652@miuegypt.edu.eg>
2023-11-24 16:26:20 +0200

    delete wishlist

--- php/wishlistclass.php
+++ php/wishlistclass.php
@@ -27,7 +27,15 @@ class wishlist{
         $this->Course_ID = $courseId;
         $this->User_ID = $userId;
 
+        $chck_existing= "SELECT * FROM wishlist  WHERE Course_ID='$course_Id' AND User_ID='$user_Id' ";
+        $chck_existing_run= mysqli_query($conn,$chck_existing);
        
+        if(mysqli_num_rows( $chck_existing_run) > 0 )
+        {
+            echo "Already added ";
+        }
+        else
+        {
         $query = "INSERT INTO wishlist (Course_ID ,User_ID) VALUES ('$courseId', '$userId' )";
         
         $stmt = $this->connect->prepare($query);
@@ -38,10 +46,27 @@ class wishlist{
         } else {
             echo "Error adding to wishlist: " . $stmt->error;
         }
-
-      
+   
+        }
     }
+
+    public function deletewishlist($courseId,$userId)
+    {
+        $this->Course_ID = $courseId;
+        $this->User_ID = $userId;
+
+        $chck_existing= "SELECT * FROM wishlist  WHERE Course_ID='$course_Id' AND User_ID='$user_Id' ";
+        $chck_existing_run= mysqli_query($conn,$chck_existing);
+       
+        if(mysqli_num_rows( $chck_existing_run) > 0 )
+{
+    $query = "DELETE FROM wishlist (Course_ID ,User_ID) VALUES ('$courseId', '$userId' )";
+        
+        $stmt = $this->connect->prepare($query);
 }
+    }
+    }
+
 
  
 ?>
