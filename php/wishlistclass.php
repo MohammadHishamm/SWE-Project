7ef0951a872394052a4588ad4cc7118f9d24ee51
Omar Hosny <omar2108721@miuegypt.edu.eg>
2023-11-25 19:38:41 +0200

    wishlist duplicates

--- php/wishlistclass.php
+++ php/wishlistclass.php
@@ -27,16 +27,15 @@ class wishlist{
         $this->Course_ID = $courseId;
         $this->User_ID = $userId;
 
-        $chck_existing= "SELECT * FROM wishlist  WHERE Course_ID='$course_Id' AND User_ID='$user_Id' ";
-        $chck_existing_run= mysqli_query($conn,$chck_existing);
+        $chck_existing= "SELECT * FROM wishlist WHERE Course_ID = '$courseId' AND User_ID = '$userId'";
+        $stmt1 = $this->connect->prepare($chck_existing);
+        $stmt1->execute();
+
+        if ($stmt1->fetch()) {
+            echo "Already added";
+        } else {
        
-        if(mysqli_num_rows( $chck_existing_run) > 0 )
-        {
-            echo "Already added ";
-        }
-        else
-        {
-        $query = "INSERT INTO wishlist (Course_ID ,User_ID) VALUES ('$courseId', '$userId' )";
+         $query = "INSERT INTO wishlist (Course_ID ,User_ID) VALUES ('$courseId', '$userId' )";
         
         $stmt = $this->connect->prepare($query);
       
@@ -46,8 +45,8 @@ class wishlist{
         } else {
             echo "Error adding to wishlist: " . $stmt->error;
         }
-   
-        }
+    }
+        
     }
 
     public function deletewishlist($courseId,$userId)
