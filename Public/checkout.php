2f56ef611dcafda056cbfc0c78dd9eb91a41527e
Mervat Sherif <Mervat2105283@miuegypt.edu.eg>
2023-12-19 18:34:36 +0200

    checkout

--- Public/checkout.php
+++ Public/checkout.php
@@ -42,7 +42,7 @@ $View_checkout = new ViewCheckout($checkout_controller,$checkout_model);
       href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css"
     />
 
-    <link rel="stylesheet" href="../css/wishlist.css" />
+    <link rel="stylesheet" href="../css/checkout.css" />
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
