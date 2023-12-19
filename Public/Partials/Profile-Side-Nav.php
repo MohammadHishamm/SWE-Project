94d34495e03303bd614a1c4df4917b152bcd3f4b
Omar Hosny <omar2108721@miuegypt.edu.eg>
2023-12-19 14:29:18 +0200

    profile sidenav fix

--- Public/Partials/Profile-Side-Nav.php
+++ Public/Partials/Profile-Side-Nav.php
@@ -31,35 +31,35 @@ if (isset($_SESSION['user_data'])) {
             </li>
             <?php
         }
+        else if($type=='Teacher')
+        {
+            ?>
+            <li class="list-group-item d-flex justify-content-start align-items-start pt-3">
+                <a href="Edit-Profile.php" class="btn btn-light mb-0 w-100 active" style="">Profile</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-start align-items-start pt-3">
+                <a href="Edit-Profile-Email.php" class="btn btn-light w-100 mb-0" style="">Account</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-start align-items-start pt-3 w-100">
+                <div class="dropdown w-100">
+                    <a class="btn btn-light w-100 dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                       data-mdb-dropdown-init data-mdb-ripple-init aria-expanded="false">My Courses
+                    </a>
+                    <ul class="dropdown-menu w-100 text-center" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="Control-Courses.php?action=add">Add Courses</a></li>
+                        <li><a class="dropdown-item" href="Control-Courses.php?action=view">View Courses</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="list-group-item d-flex justify-content-start align-items-start pt-3">
+                <a href="Control-Content.php?action=add" class="btn btn-light w-100 mb-0" style="">Add Content</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-start align-items-start pt-3">
+                <a href="Control-Content.php?action=view" class="btn btn-light w-100 mb-0" style="">View Content</a>
+            </li>
+            <?php
+        }
     }
-}
-else {
-    // If the condition is not met, display the entire list
-    ?>
-    <li class="list-group-item d-flex justify-content-start align-items-start pt-3">
-        <a href="Edit-Profile.php" class="btn btn-light mb-0 w-100 active" style="">Profile</a>
-    </li>
-    <li class="list-group-item d-flex justify-content-start align-items-start pt-3">
-        <a href="Edit-Profile-Email.php" class="btn btn-light w-100 mb-0" style="">Account</a>
-    </li>
-    <li class="list-group-item d-flex justify-content-start align-items-start pt-3 w-100">
-        <div class="dropdown w-100">
-            <a class="btn btn-light w-100 dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-               data-mdb-dropdown-init data-mdb-ripple-init aria-expanded="false">My Courses
-            </a>
-            <ul class="dropdown-menu w-100 text-center" aria-labelledby="dropdownMenuLink">
-                <li><a class="dropdown-item" href="Control-Courses.php?action=add">Add Courses</a></li>
-                <li><a class="dropdown-item" href="Control-Courses.php?action=view">View Courses</a></li>
-            </ul>
-        </div>
-    </li>
-    <li class="list-group-item d-flex justify-content-start align-items-start pt-3">
-        <a href="Control-Content.php?action=add" class="btn btn-light w-100 mb-0" style="">Add Content</a>
-    </li>
-    <li class="list-group-item d-flex justify-content-start align-items-start pt-3">
-        <a href="Control-Content.php?action=view" class="btn btn-light w-100 mb-0" style="">View Content</a>
-    </li>
-    <?php
 }
 ?>
 
